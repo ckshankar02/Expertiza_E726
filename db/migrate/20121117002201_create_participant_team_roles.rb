f0933931accfb41bb861d4fc3e403343f7946d27
sravan <sparnan@ncsu.edu>
2012-11-16 21:38:28 -0500

    showing 60 tables

--- db/migrate/20121117002201_create_participant_team_roles.rb
+++ db/migrate/20121117002201_create_participant_team_roles.rb
@@ -1,13 +1,14 @@
 class CreateParticipantTeamRoles < ActiveRecord::Migration
   def self.up
     create_table :participant_team_roles do |t|
-      t.integer :roleset_assignment_id
+      t.integer :id
+      t.integer :role_assignment_id
       t.integer :participant_id
       t.timestamps
     end
     execute "ALTER TABLE `participant_team_roles`
-    ADD CONSTRAINT fk_roleset_assignment_id
-    FOREIGN KEY (roleset_assignment_id) references team_roleset_assignments(id)"
+    ADD CONSTRAINT fk_role_assignment_id
+    FOREIGN KEY (role_assignment_id) references teamrole_assignment(id)"
     execute "ALTER TABLE `participant_team_roles`
     ADD CONSTRAINT fk_participant_id
     FOREIGN KEY (participant_id) references participants(id)"
@@ -15,7 +16,7 @@ class CreateParticipantTeamRoles < ActiveRecord::Migration
 
   def self.down
     execute "ALTER TABLE `participant_team_roles`
-    DROP FOREIGN KEY fk_roleset_assignment_id"
+    DROP FOREIGN KEY fk_role_assignment_id"
     execute "ALTER TABLE `participant_team_roles`
     DROP FOREIGN KEY fk_participant_id"
     drop_table :participant_team_roles
